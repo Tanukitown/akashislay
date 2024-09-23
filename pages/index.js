503d8f054e4c518d69337be26a9296abe5c85166
Tanukitown <kevin.mcguffin@gmail.com>
2024-09-23 14:54:31 -0400

    There is no such thing as a coincidence

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
       </Head>
 
       <main>
+        <h5>There is no such thing as a coincidence</h5>
         <img src="https://cdn.discordapp.com/attachments/959857101860122646/1264724381980954705/SPOILER_gotchya.png?ex=66f2a11e&is=66f14f9e&hm=400ad68992911063226af555ea321d95012c63a69e391278d72db520423d89cb&" />
       </main>
     </div>
