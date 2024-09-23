9204cd9a106e49fd656f458a6415cdadeb9aa4bd
Tanukitown <kevin.mcguffin@gmail.com>
2024-09-23 11:59:33 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   return (
@@ -13,8 +12,6 @@ export default function Home() {
       <main>
         <img src="https://cdn.discordapp.com/attachments/959857101860122646/1264724381980954705/SPOILER_gotchya.png?ex=66f2a11e&is=66f14f9e&hm=400ad68992911063226af555ea321d95012c63a69e391278d72db520423d89cb&" />
       </main>
-
-      <Footer />
     </div>
   )
 }
