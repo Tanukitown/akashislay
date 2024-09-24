b24b902ef9c346fbea337c9c3bd6eb9d36acc4b3
Tanukitown <kevin.mcguffin@gmail.com>
2024-09-23 22:24:26 -0400

    lint fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ const Application = ({ Component, pageProps }) => {
         <Component {...pageProps} />
       </div>
       <aside id="aside" className="col-span-12 md:col-span-4 p-4">
-        <div>Akashi's post of the day:</div>
+        <div>Akashi&apos;s post of the day:</div>
         <div>There is no such thing as coincidence</div>
       </aside>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,8 @@ const Home = () => {
       <p>
         As of 2020, we have now renounced our Genbu Slayer ways. We now fight
         for the turtles, in hope that they will bring us fortune and luck.
-        Please, do not kill turtles. They have feelings, and you're a bully if
-        you think otherwise. #Turtles #LiveLoveLaugh #Genburgers
+        Please, do not kill turtles. They have feelings, and you&apos;re a bully
+        if you think otherwise. #Turtles #LiveLoveLaugh #Genburgers
         #WholeFoods?MoreLikeWholeCheck! #MyDogIsVegan #MyTurtleIsVegan
         #AreYouStillReadingThis? #Meneil
       </p>
