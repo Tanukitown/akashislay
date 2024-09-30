9cdf1ccbaca83dcdddceed06747bb13b319cefe7
Tanukitown <kevin.mcguffin@gmail.com>
2024-09-30 12:54:36 -0400

    Promotion girl

---
+++ pages/clapsforari/index.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+const About = () => {
+  return (
+    <>
+      <Image
+        src="/images/clapping.gif"
+        alt="claps for ari"
+        height={1920}
+        width={1080}
+      />
+    </>
+  );
+};
+
+export default About;
